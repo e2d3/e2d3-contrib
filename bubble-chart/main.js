14002950c978b1080a864b3c509312895acd920f
upsilon2 <murakami.yasuo@gmail.com>
2015-03-17 19:44:45 +0900

    bubble chart for E2D3 ver.0.4

---
+++ bubble-chart/main.js
@@ -0,0 +1,10 @@
+define(['e2d3bridge', 'original'], function (bridge, original) {
+  return function (node, baseUrl) {
+    return {
+      update: function (data) {
+        show(data.toList());
+//        show(data);
+      }
+    };
+  };
+});
