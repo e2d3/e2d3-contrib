0b4409e3202f3f75c8b105ce355e210633ae88e8
Sawa <nakamods320yen@gmail.com>
2015-03-30 00:35:03 +0900

    add UI to switch data properties

--- population-map-iran/main.js
+++ population-map-iran/main.js
@@ -3,6 +3,8 @@
 var width = root.clientWidth;
 var height = root.clientHeight;
 
+var data_prop = 'Population';
+
 var svg = d3.select(root).append('svg')
   .attr('width', width)
   .attr('height', height)
@@ -41,10 +43,10 @@ function update(data) {
 
   var tsv = data.toList();
   tsv.forEach(function(d){
-    d.Population = +d.Population;
+    d[data_prop] = +d[data_prop];
   });
   var color = d3.scale.linear()
-    .domain(d3.extent(tsv, function(d){return d.Population}))
+    .domain(d3.extent(tsv, function(d){return d[data_prop]}))
     .range(['#ffffff', '#ff0000'])
     .interpolate(d3.interpolateLab);
 
@@ -52,9 +54,34 @@ function update(data) {
     .attr('fill', function (d) {
       var obj = tsv.filter(function(e){return e.Province==d.properties.name})[0];
       if (obj) {
-        return color(obj.Population);
+        return color(obj[data_prop]);
       } else {
         return '#cccccc';
       }
     });
+}
+showSwitchButtons();
+function showSwitchButtons() {
+  var labels = ['Population', 'Share of Population', 'Population Average Annul Growth', 'Internet Rate', 'Unemployment Rate', 'Economic Participation Rate', 'Contribution of GDP'];
+
+  var switch_box = d3.select(root)
+    .append('div')
+      .attr('id', 'switch_box')
+      .attr('style', 'position:absolute;top:0');
+
+  switch_box.selectAll('.switch_button')
+    .data(labels).enter()
+      .append('div')
+        .attr('class', 'switch')
+        .html(function(d, i){ 
+          return '<input type="radio" id="switch_radio_'+i+'" name="switch_radio" class="switch_radio" value="'+d+'" />'
+                +'<label class="switch_label" for="switch_radio_'+i+'">'+d+'</label>'
+        });
+
+  switch_box.selectAll('.switch_radio').on('click', function(){
+    data_prop = d3.select(this).attr('value');
+  });
+
+
+
 }
