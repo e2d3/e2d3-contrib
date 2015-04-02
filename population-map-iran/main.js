1d580f5e7136a41f5e141dd63ef39e6bd17766fc
Sawa <nakamods320yen@gmail.com>
2015-04-02 23:53:45 +0900

    change colors by data

--- population-map-iran/main.js
+++ population-map-iran/main.js
@@ -36,29 +36,38 @@ d3.json(baseUrl + '/ir.topojson', function (error, json) {
 });
 
 function update(data) {
-  var map = data.toMap();
-  var initIndex = 2;
-  var initLabel = map.header[initIndex];
-  var values = map.values();
+  function render(){
+    var map = data.toMap();
+    var initIndex = 2;
+    var initLabel = map.header[initIndex];
+    var values = map.values();
 
-  var tsv = data.toList();
-  tsv.forEach(function(d){
-    d[data_prop] = +d[data_prop];
-  });
-  var color = d3.scale.linear()
-    .domain(d3.extent(tsv, function(d){return d[data_prop]}))
-    .range(['#ffffff', '#ff0000'])
-    .interpolate(d3.interpolateLab);
-
-  svg.selectAll('.states')
-    .attr('fill', function (d) {
-      var obj = tsv.filter(function(e){return e.Province==d.properties.name})[0];
-      if (obj) {
-        return color(obj[data_prop]);
-      } else {
-        return '#cccccc';
-      }
+    var tsv = data.toList();
+    tsv.forEach(function(d){
+      d[data_prop] = +d[data_prop];
     });
+    var color = d3.scale.linear()
+      .domain(d3.extent(tsv, function(d){return d[data_prop]}))
+      .range(['#ffffff', '#ff0000'])
+      .interpolate(d3.interpolateLab);
+
+    svg.selectAll('.states')
+      .attr('fill', function (d) {
+        var obj = tsv.filter(function(e){return e.Province==d.properties.name})[0];
+        if (obj) {
+          return color(obj[data_prop]);
+        } else {
+          return '#cccccc';
+        }
+      });
+  }
+
+  d3.selectAll('.switch_radio').on('click', function(){
+    data_prop = d3.select(this).attr('value');
+    render();
+  });
+  render();
+
 }
 showSwitchButtons();
 function showSwitchButtons() {
@@ -77,11 +86,4 @@ function showSwitchButtons() {
           return '<input type="radio" id="switch_radio_'+i+'" name="switch_radio" class="switch_radio" value="'+d+'" />'
                 +'<label class="switch_label" for="switch_radio_'+i+'">'+d+'</label>'
         });
-
-  switch_box.selectAll('.switch_radio').on('click', function(){
-    data_prop = d3.select(this).attr('value');
-  });
-
-
-
 }
