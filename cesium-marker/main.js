4141ab260213a0e3d43094ea598a7df0204e2dbc
yuuu <yuuu@pita-ri.jp>
2015-06-14 18:34:12 +0900

    Delete auto rewrited text by git.

--- cesium-marker/main.js
+++ cesium-marker/main.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
-//# require=d3,c=Cesium
-=======
 //# require=d3,Cesium
->>>>>>> 1124bbe25d4965448fcddf48504a0c328ed9cc48
-
 
 //Cesiumが読み込まれているか確認
 console.log("installed", Cesium);
 
-//Cesium用 divの追加 
+//Cesium用 divの追加
 var cesiumContainer = d3.select("#e2d3-chart-area")
 	.append('div')
 	.attr('id', 'cesiumContainer')
@@ -21,7 +16,7 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 function update(data) {
 	//Excelから取得したデータをGeoJSONにする
 	var geojson =  toPointGeoJSON(toObjectArray(data));
-	 
+
 
 	//現在表示されているマーカーを削除する
 	viewer.dataSources.removeAll();
@@ -35,23 +30,23 @@ function update(data) {
 
 	//マーカー表示
 	viewer.dataSources.add(Cesium.GeoJsonDataSource.load(geojson));
-	
+
 	//スタート地点へズーム
 	var lnglat = geojson.features[0].geometry.coordinates;
-	panTo(lnglat[0], lnglat[1])	
+	panTo(lnglat[0], lnglat[1])
 }
 
 
 function panTo(lng, lat){
 	viewer.camera.flyTo({
 		destination : Cesium.Cartesian3.fromDegrees(lng, lat, 700000)
-	});		
+	});
 }
 
 //[{key:value},{key:value}]の形式に変換
 function toObjectArray(array){
   var header = array.shift();
-  var objArray = []; 
+  var objArray = [];
   for(var b=0; b < array.length; b++){
     var tmp = {};
     for(var i=0; i< header.length; i++){
@@ -68,7 +63,7 @@ function toPointGeoJSON(dataObj){
 		'type': 'FeatureCollection',
 		'features': []
 	}
-	
+
 	var pointTemplate = {
 		'type': 'Feature',
 		'properties': {},
@@ -77,15 +72,15 @@ function toPointGeoJSON(dataObj){
 			'coordinates': [null,null]
 		}
     }
-	
+
 	var points = dataObj.map(function(d){
 		var point =JSON.parse(JSON.stringify(pointTemplate));
 		point.properties = d;
 		point.geometry.coordinates = [d['経度'], d['緯度']];
 		return point
 	});
-	
+
 	container.features = points;
-	
+
 	return container;
 }
