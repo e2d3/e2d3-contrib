7d9e48bf5f0c49abae05f118aadb75095d2317de
yuuu <yuuu@pita-ri.jp>
2015-09-12 16:37:26 +0900

    lig hack first

---
+++ food-analyzer/main.js
@@ -0,0 +1,41 @@
+//# require=d3
+
+var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+    width = root.clientWidth - margin.left - margin.right,
+    height = root.clientHeight - margin.top - margin.bottom,
+    imageSise = 100;
+
+var color = d3.scale.category10c();
+
+var treemap = d3.layout.treemap()
+    .size([width, height])
+    .value(function(d) { return d.size; });
+
+var div = d3.select(root).append("div")
+    .style("position", "relative")
+    .style("width", (width + margin.left + margin.right) + "px")
+    .style("height", (height + margin.top + margin.bottom) + "px")
+    .style("left", margin.left + "px")
+    .style("top", margin.top + "px");
+
+
+function update(data) {
+  div.selectAll('.node').remove();
+
+  var data = data.toNested().children[0];
+
+  var node = div.datum(data).selectAll(".node")
+      .data(treemap.nodes)
+    .enter().append("div")
+      .attr("class", "node")
+      .call(position)
+      .style("background", function(d) { return d.children ? color(d.name) : null; })
+      .text(function(d) { return d.children ? null : d.name; });
+}
+
+function position() {
+  this.style("left", function(d) { return d.x + "px"; })
+    .style("top", function(d) { return d.y + "px"; })
+    .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+    .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
+}
