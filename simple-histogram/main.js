6bf3d1145194ffabb83f28cf442e9b9fc7989f9e
keisuke-osone <paradi.bass@gmail.com>
2016-04-29 15:19:53 +0900

    data modified and source refactoring

--- simple-histogram/main.js
+++ simple-histogram/main.js
@@ -1,5 +1,6 @@
 //# require=d3
-var margin = { top: 20, right: 30, bottom: 30, left: 40 };
+var DEFAULT_BIN = 50;
+var margin = { top: 20, right: 30, bottom: 40, left: 40 };
 var width = root.clientWidth - margin.left - margin.right;
 var height = root.clientHeight - margin.top - margin.bottom;
 
@@ -13,7 +14,7 @@ var chart = d3.select(root).append('svg')
     .attr('height', root.clientHeight)
   .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
+var Logarithm = false;
 
 function update(data) {
   var list = data.transpose();
@@ -21,32 +22,40 @@ function update(data) {
   var key = list[1][0];
   target = list[1];
   target.splice(0, 1)
-  target = target.map(function(d) {return Math.log10(parseInt(d))});
-  console.log(target);
+  if (Logarithm) {
+    target = target.map(function(d) {return Math.log10(parseInt(d));});
+  } else {
+    target = target.map(function(d) {return parseInt(d);});
+  }
 
   if (!env.colors()) env.colors(d3.scale.category10().range());
   var color = d3.scale.ordinal().range(env.colors());
   var values = target;
-  
 
-
-  x.domain([0, d3.max(target, function(d) { return d; })])
+  x.domain([d3.min(target, function(d) { return d; }), d3.max(target, function(d) { return d; })])
   var list = d3.layout.histogram()
-      .bins(x.ticks(100))
+      .bins(x.ticks(DEFAULT_BIN))
       (values);
   var y = d3.scale.linear()
     .domain([0, d3.max(list, function(d) { return d.y; })])
     .range([height, 0]);
-
-  console.log(d3.max(list, function(d) { return d.x; }));
-
+  
+  var barWidth = parseInt(width / x.ticks(DEFAULT_BIN).length);
   var setup = function (selection) {
     selection
         .attr('class', 'bar')
-        .attr('x', function (d) { return x(d.x); })
-        .attr('y', function (d) { return y(d.y); })
-        .attr('height', function (d) { return height - y(d.y); })
-        .attr('width', x(list[0].dx) - 1)
+        .attr('x', function (d) {
+            return x(d.x);
+        })
+        .attr('y', function (d) {
+            return y(d.y);
+        })
+        .attr('height', function (d) {
+            return height - y(d.y);
+        })
+        .attr('width', function () {
+            return barWidth;
+        })
         .style('fill', function (d) { return color(d.name); });
   }
 
@@ -59,7 +68,19 @@ function update(data) {
   chart.append('g')
       .attr('class', 'x axis')
       .attr('transform', 'translate(0,' + height + ')')
-      .call(xAxis);
+      .call(xAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", 26)
+      .style("text-anchor", "end")
+      .text(function () {
+        if (Logarithm) {
+          return 'log10' + key + '';
+        } else {
+          return key;
+        }
+      });
 
 
   var yAxis = d3.svg.axis()
@@ -69,18 +90,15 @@ function update(data) {
   chart.append('g')
       .attr('class', 'y axis')
       .call(yAxis)
-    .append('text')
+      .append('text')
       .attr('transform', 'rotate(-90)')
       .attr('y', 6)
       .attr('dy', '.71em')
       .style('text-anchor', 'end')
-      .text(key);
+      .text('frequency');
 
   rect = chart.selectAll('.bar').data(list);
-
   rect.transition().duration(500).call(setup);
-
   rect.enter().append('rect').call(setup);
-
   rect.exit().remove();
 }
