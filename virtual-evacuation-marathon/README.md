141da8f44afa4dc2da6b58c81aa5f32c106fe4c6
ktakana <katsutoshi.takanashi.ze@hitachi-solutions.com>
2017-03-04 18:26:02 +0900

    避難訓練アプリ
    tcsデータをE2D3データに変換する機能を作りました。

---
+++ virtual-evacuation-marathon/README.md
@@ -0,0 +1,7 @@
+Virtual Evacuation Marathon
+====
+
+|name|HH|MM|SS|Color(RGB)|
+|--|--|--|--|--|
+|山田|06|25|23|ff5588|
+|...|...|...|...|...|
---
+++ virtual-evacuation-marathon/main_1.js
@@ -0,0 +1,299 @@
+//# require=d3
+
+var margin = {
+	top: 0,
+	right: 0,
+	bottom: 0,
+	left: 0
+};
+
+//pathdata
+var pathdata = "M16,380 L10,348 L36,340 L64,348 L70,330 L110,325 L194,350 L246,360 L266,357 L285,363 L321,346 L342,344 L374,346 L392,356 L416,361 L474,349 L485,344 L500,328 L566,224 L583,210 L636,266 L683,276 L693,294 L752,294 L766,310 L777,316 L796,318 L849,298 L904,450 L896,470 L960,501 L974,470 L1040,434 L998,362 L1007,353 L1014,288 L1068,202 L1090,182 L1121,145 L1134,116 L1150,96 L1153,68 L1170,76 L1150,110 L1140,122 L1126,152 L1115,168 L1076,206 L1067,216 L1100,223 L1128,235 L1146,236 L1170,513 L1137,567 L1140,596 L1130,608 L1172,631 L1176,637 L1167,640 L1119,612 L1132,592 L1128,569 L1130,568 L1140,547 L1162,510 L1156,468 L1140,244 L1116,241 L1092,230 L1066,226 L1021,292 L1016,351 L1015,358 L1009,364 L1052,436 L981,475 L966,510 L890,476 L851,560 L798,626 L738,584 L722,598 L699,650 L651,772 L628,941 L566,983 L547,1027 L530,1064 L511,1138 L506,1142 L502,1132 L516,1078 L536,1033 L550,991 L567,971 L618,935 L644,766 L716,594 L733,577 L742,575 L753,583 L782,482 L766,478";
+//size
+var width = root.clientWidth - margin.left - margin.right;
+var height = root.clientHeight - margin.top - margin.bottom;
+
+//初期データを登録する。
+	var rr = 25;
+	var ref1 = 8;
+	eval ("c" + 1 + "= [\"デニス\",\"02\",\"02\",\"57\",\"2398ff\",0, 0, rr,\"id1\"]");
+	eval ("c" + 2 + "= [\"ポーラ\",\"02\",\"15\",\"25\",\"2398ff\",0, 0, rr,\"id2\"]");
+	eval ("c" + 3 + "= [\"サブ３\",\"03\",\"00\",\"00\",\"2398ff\",0, 0, rr,\"id3\"]");
+
+	var arrInitial = new Array();
+	arrInitial.push(c1);
+	arrInitial.push(c2);
+	arrInitial.push(c3);
+
+	movecircle(arrInitial);
+
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+function update(data) {
+
+rr = 25;
+
+var arrInitial = new Array();
+
+	var arrTemp = new Array();
+	arrTemp.push("サブ３");
+	arrTemp.push("03");
+	arrTemp.push("00");
+	arrTemp.push("00");
+	arrTemp.push("ff0000");
+	arrTemp.push("0");
+	arrTemp.push("0");
+	arrTemp.push(rr);
+	arrTemp.push("id0");
+	arrInitial.push(arrTemp);
+
+for (var i=1 ; i<=data.length -1 ; i++){
+	var arrTemp = new Array();
+	arrTemp.push(eval("data[" + i + "][0]"));
+	arrTemp.push(eval("data[" + i + "][1]"));
+	arrTemp.push(eval("data[" + i + "][2]"));
+	arrTemp.push(eval("data[" + i + "][3]"));
+	arrTemp.push(eval("data[" + i + "][4]"));
+	arrTemp.push("0");
+	arrTemp.push("0");
+	arrTemp.push(rr);
+	arrTemp.push(eval("\"id" + i + "\""));
+	arrInitial.push(arrTemp);
+}
+ movecircle(arrInitial);
+}
+
+function remove() {
+	drawArea
+		.selectAll('.' + preMetrics + 'class')
+		.data({})
+		.exit()
+		.transition()
+		.duration(4000)
+		.ease('linear')
+		.attr('x', function(d, i) {
+			return -width;
+		})
+		.remove()
+
+	drawArea
+		.selectAll('.' + preMetrics + 'label')
+		.data({})
+		.exit()
+		.transition()
+		.duration(4000)
+		.ease('linear')
+		.attr('x', function(d, i) {
+			return -width;
+		})
+		.remove()
+}
+
+function drawBackground(svg, width, height, margin) {
+	var background = svg.append('g');
+
+	//background-linearGradient-sea
+	background.append('linearGradient')
+		.attr({
+			id: 'sGradient',
+			'gradientUnits': 'objectBoundingBox',
+			'gradientTransform': 'rotate(90)'
+		})
+		.selectAll('stop')
+		.data([{
+			offset: "100%",
+			color: "rgb(0,0,0)",
+			opacity: "0.2"
+		}, {
+			offset: "100%",
+			color: "rgb(0,0,0)",
+			opacity: "1"
+		}])
+		.enter()
+		.append('stop')
+		.attr("offset", function(d) {
+			return d.offset;
+		})
+		.attr("stop-color", function(d) {
+			return d.color;
+		})
+		.attr("stop-opacity", function(d) {
+			return d.opacity;
+		});
+
+	// backbround
+	background
+		.append('rect')
+		.attr({
+			class: 'background',
+			width: width,
+			height: height,
+			fill: 'url(#sGradient)'
+		});
+
+	// backbround-ground
+	background
+		.append('rect')
+		.attr({
+			class: 'background',
+			width: width,
+			height: margin.bottom,
+			//fill: 'url(#gGradient)',
+			y: height - margin.bottom
+		});
+}
+
+	// 動かしたい円を指定する
+function movecircle(data){
+//draw background-image (should be called before base-svg creation)
+d3.select(root).append('img')
+	.attr({
+		width: width,
+		height: root.clientHeight,
+		src: baseUrl + '/map.jpg',
+	})
+	.style({
+		position: 'absolute',	
+		top: 0, //margin.top,
+		left: 0, //margin.left
+	});
+
+//create base-svg
+var svg = d3.select(root).append('svg')
+	.attr({
+		width: width,
+		height: height
+	})
+	.style({
+		position: 'absolute',
+		top: 0, //margin.top,
+		left: 0, //margin.left
+	});
+
+//draw background svg
+drawBackground(svg, width, height, margin);
+
+//define draw area
+var drawArea = svg
+	.append('g')
+	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+// defs/markerという構造で、svgの下に矢印を定義
+	var marker = drawArea.append("defs").append("marker")
+//	var marker = d3.select(root).append("defs").append("marker")
+		.attr({
+		'id': "arrowhead",
+		'refX': ref1,
+		'refY': 2,
+		'markerWidth': 4,
+		'markerHeight': 4,
+		'orient': "auto"
+		});
+	// 矢印の形をpathで定義します。
+	marker.append("path")
+		.attr({
+		d: "M 0,0 V 4 L4,2 Z",
+		fill: "steelblue"
+		});
+
+	// 10種類の色を返す関数を使う
+	var color = d3.scale.category10();
+	var g = drawArea.selectAll('g')
+//	var g = d3.select(root).selectAll('g')
+	.data(data).enter().append('g')
+	.attr({
+	// 座標設定を動的に行う
+		id: function(d) { return d[8]; },
+		transform: function(d) {
+	return "translate(" + d[5] + "," + d[6] + ")";
+		},
+	});
+	g.append('circle')
+	.attr({
+		'r': function(d,i) { return parseInt(data[i][7]); },
+		'fill': function(d,i) { return color(i); },
+	});
+
+	g.append('text')
+	.attr({
+		'text-anchor': "middle",
+		'dy': ".35em",
+		'fill': 'black',
+	})
+	.text(function(d,i) { return data[i][0]; });
+
+	var line = d3.svg.line()
+
+	var path = svg.append('path')
+		.attr({
+		'd': line(arrInitial),
+		'id': 'nodepath',
+		'stroke': 'lightgreen',
+		'stroke-width': 5,
+		'fill': 'none',
+		});
+	var t = path.node().getTotalLength();
+	var tdiff = t - (rr+rr+ref1);
+	path.attr({
+	'stroke-dasharray': "0 " + rr + " " + tdiff + " " + rr,
+	'stroke-dashoffset': 0,
+	});
+	var t2 = t*2;
+	
+	for(var j=1; j<200; j++) {
+var str = "M102,963 L94,1003 L119,1038 L150,1018 L192,1035 L247,1020 L302,1005 L360,998 L418,1005 L475,1018 L531,1003 L585,1001 L638,1027 L672,1073 L708,1127 L741,1175 L786,1139 L830,1105 L872,1078 L927,1074 L976,1050 L1026,1071 L1063,1045 L1081,990 L1101,935 L1109,880 L1140,846 L1186,837 L1223,882 L1275,912 L1250,957 L1239,1011 L1244,1070 L1271,1124 L1302,1176 L1345,1219 L1375,1270 L1392,1293 L1361,1242 L1324,1197 L1304,1161 L1356,1145 L1392,1109 L1397,1049 L1401,988 L1405,928 L1410,868 L1408,809 L1379,757 L1370,704 L1415,677 L1368,703 L1377,758 L1406,808 L1408,866 L1404,925 L1400,984 L1396,1044 L1391,1102 L1364,1141 L1309,1158 L1270,1121 L1248,1073 L1239,1013 L1249,956 L1276,908 L1225,877 L1192,843 L1149,839 L1099,862 L1074,808 L1046,755 L1012,710 L970,710 L930,744 L896,698 L869,645 L850,588 L829,533 L814,476 L807,416 L800,356 L775,310 L728,273 L701,220 L679,165 L664,110 L663,115 L680,171 L703,224 L728,278 L773,312 L797,355 L805,409 L811,469 L825,526 L847,581 L868,637 L888,689 L919,739 L951,759 L965,818 L958,859";
+
+	var path3 = svg.append("path")
+	.attr({
+		'd': str,
+		'fill': "none",
+		'transform': "translate("+data[1][5]/(1200/width)+","+data[1][6]/(1200/root.clientHeight)+")",
+	});
+
+for (var j=1 ; j<data.length  ; j++){
+	eval("drawArea.selectAll('#id" + j  + "')")
+	.transition()
+	// x秒かけて一周させる
+	.duration((parseInt(data[j][1]) * 3600 + parseInt(data[j][2]) * 60 + parseInt(data[j][3]))* 10)
+	.ease("linear")
+	.attrTween(
+	// 座標設定を動的に行う
+		'transform', function(d,j) {
+		return function(t) {
+			// path(ここでは円)の座標を取得
+			var p = path3.node().getPointAtLength(path3.node().getTotalLength()*t);
+			data[1][5] = (data[j][5]+p.x)/(1200/width);
+			data[1][6] = (data[j][6]+p.y)/(1200/root.clientHeight);
+			return "translate(" + data[1][5] + "," +data[1][6] + ")";
+		};
+		}
+	)
+
+}
+
+var j = 0;
+	eval("drawArea.selectAll('#id" + j  + "')")
+	.transition()
+	// x秒かけて一周させる
+	.duration((parseInt(data[j][1]) * 3600 + parseInt(data[j][2]) * 60 + parseInt(data[j][3]))* 10)
+	// easeを指定すると、transitionで変化させるパラメータを[0->1]にすることができる。
+	.ease("linear")
+	.attrTween(
+	// 座標設定を動的に行う
+		'transform', function(d,j) {
+		// easeで設定したパラメータがtとなって渡ってくる
+		return function(t) {
+			// path(ここでは円)の座標を取得
+			var p = path3.node().getPointAtLength(path3.node().getTotalLength()*t);
+			data[1][5] = (data[j][5]+p.x)/(1200/width);
+			data[1][6] = (data[j][6]+p.y)/(1200/root.clientHeight);
+			return "translate(" + data[1][5] + "," +data[1][6] + ")";
+		};
+		}
+	)
+	.each("end", function() {movecircle(data)});
+
+	
+	};
