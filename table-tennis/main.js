629cf0794f84076e63975c22ab91a38332ff3d2f
えばたあや <ayaaaaa.0u0w@gmail.com>
2017-03-04 17:09:54 +0900

    remove いらないもの

--- table-tennis/main.js
+++ table-tennis/main.js
@@ -1,22 +1,6 @@
 //# require=d3
 
 var margin = { top: 20, right: 30, bottom: 60, left: 80 };
-var width = root.clientWidth - margin.left - margin.right;
-var height = root.clientHeight - margin.top - margin.bottom;
-
-var x = d3.scale.ordinal()
-  .rangeRoundBands([0, width], .1)
-
-var y = d3.scale.linear()
-  .rangeRound([height, 0])
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient('bottom');
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient('left')
 
 d3.select(root)
   .append('div')
