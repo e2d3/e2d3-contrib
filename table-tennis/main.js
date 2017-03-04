a0a763702babeae889952ee9df12b319e67d0672
えばたあや <ayaaaaa.0u0w@gmail.com>
2017-03-04 13:52:04 +0900

    update main.js

--- table-tennis/main.js
+++ table-tennis/main.js
@@ -1,1 +1,29 @@
-aaa
+//# require=d3
+
+var margin = { top: 20, right: 30, bottom: 60, left: 80 };
+var width = root.clientWidth - margin.left - margin.right;
+var height = root.clientHeight - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+  .rangeRoundBands([0, width], .1)
+
+var y = d3.scale.linear()
+  .rangeRound([height, 0])
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom');
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left')
+
+var chart = d3.select(root).append('svg')
+    .attr('width', root.clientWidth)
+    .attr('height', root.clientHeight)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+function update(data) {
+    console.log(data);
+}
